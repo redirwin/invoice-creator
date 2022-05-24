318f18dc5458952241413805911ec8a0e95b2de2
Dave Irwin <david@redirwin.com>
2022-05-23 17:40:31 -0700

    Successfully displaying total.

--- index.js
+++ index.js
@@ -14,17 +14,30 @@ let servicesRequested = [
 ];
 
 const taskList = document.querySelector("#task-list");
+const addCarBtn = document.querySelector("#add-car-btn");
+const addLawnBtn = document.querySelector("#add-lawn-btn");
+const addWeedsBtn = document.querySelector("#add-weeds-btn");
+const totalAmountEl = document.querySelector("#total-amount-el");
+let totalCost = 0;
 
-renderTaskList()
+renderTaskList();
+
+console.log(totalAmountEl.innerHTML);
 
 function renderTaskList() {
   for (let i = 0; i < servicesRequested.length; i++) {
-    taskList.innerHTML += `<li class="task">
-        <span class="task-name"
-          >${servicesRequested[i].name}
-          <span class="remove-task">Remove</span>
-        </span>
-        <span class="task-cost">$${servicesRequested[i].cost}</span>
-      </li>`;
+    totalCost += servicesRequested[i].cost;
+
+    taskList.innerHTML += `
+        <li class="task">
+            <span class="task-name"
+            >${servicesRequested[i].name}
+            <span class="remove-task">Remove</span>
+            </span>
+            <span class="task-cost">$${servicesRequested[i].cost}</span>
+        </li>
+        `;
   }
+
+    totalAmountEl.innerHTML = `$${totalCost}`
 }
